45facdb498ca6336585d0afed7d7816471d145cc
ubuntu-18.04-1 <gaoshuming121@163.com>
2022-09-25 23:34:15 -0700

    modify main func

--- Sources/server-test3.cc
+++ Sources/server-test3.cc
@@ -22,29 +22,8 @@ int main()
 
     HttpServer svr;
 
-    // curl -v http://ip:port/json1
-    svr.GET("/json1", [](const HttpReq *req, HttpResp *resp)
-    {
-        Json json;
-        json["test"] = 123;
-        json["json"] = "test json";
-        resp->Json(json);
-    });
-
-    // curl -v http://ip:port/json2
-    svr.GET("/json2", [](const HttpReq *req, HttpResp *resp)
-    {
-        std::string valid_text = R"(
-        {
-            "numbers": [1, 2, 3]
-        }
-        )";
-        resp->Json(valid_text);
-    });
-
-    // curl -v http://ip:port/json3
-    svr.GET("/json3", [](const HttpReq *req, HttpResp *resp)
-    {
+    // curl -v http://0.0.0.0:8888/json3
+    svr.GET("/json3", [](const HttpReq *req, HttpResp *resp){
         std::string invalid_text = R"(
         {
             "strings": ["extra", "comma", ]
@@ -54,9 +33,7 @@ int main()
     });
 
     // recieve json
-    //   curl -X POST http://ip:port/json4
-    //   -H 'Content-Type: application/json'
-    //   -d '{"login":"my_login","password":"my_password"}'
+    //   curl -X POST http://0.0.0.0:8888/json4 -H 'Content-Type: application/json' -d '{"login":"inforefiner-model","password":"inforfiner_password"}'
     svr.POST("/json4", [](const HttpReq *req, HttpResp *resp){
         Json json;
         json["test"] = 123;
@@ -75,8 +52,7 @@ int main()
                              {"value", 42.99}
                        }}
         };
-        if (req->content_type() != APPLICATION_JSON)
-        {
+        if (req->content_type() != APPLICATION_JSON){
             resp->String("NOT APPLICATION_JSON");
             return;
         }
@@ -85,12 +61,10 @@ int main()
         fprintf(stderr, "Json : %s", req->json().dump(4).c_str());
     });
 
-    if (svr.start("0.0.0.0",8888) == 0)
-    {
+    if (svr.start("0.0.0.0",8888) == 0){
         wait_group.wait();
         svr.stop();
-    } else
-    {
+    } else {
         fprintf(stderr, "Cannot start server");
         exit(1);
     }
